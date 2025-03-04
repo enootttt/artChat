835983675ef6f7441dd3bafbe57d85cd50a453ce
lihuifeng <lihuifeng@f-cloud.com.cn>
2025-03-04 15:28:45 +0800

    chore(deps): vitepress文档所需准备

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,15 @@
 // https://vitepress.dev/guide/custom-theme
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import { globals } from "../vitepress";
+import type { Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import "./style.css";
 
-const define = <T>(value: T): T => value
+const define = <T>(value: T): T => value;
 export default define<Theme>({
   extends: DefaultTheme,
   enhanceApp({ app, router, siteData }) {
-    // ...
-  }
-})
+    globals.forEach(([name, Comp]) => {
+      app.component(name, Comp);
+    });
+  },
+});
